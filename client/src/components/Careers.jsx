04cbea543d7d50ba14939f8e10cb7e027ce6b340
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-06-07 22:45:12 -0600

    debugging CSS

--- client/src/components/Careers.jsx
+++ client/src/components/Careers.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { DataGrid } from '@mui/x-data-grid';
+import AddIcon from '@mui/icons-material/Add';
 
 const Careers = () => {
-  return (
-    <div>Careers</div>
-  )
-}
+  const [rows, setRows] = useState([
+    { id: 1, date: null, opportunity: '', notes: '' },
+  ]);
 
-export default Careers
+  const columns = [
+    { field: 'date', headerName: 'Date', width: 200, editable: true },
+    { field: 'opportunity', headerName: 'Opportunity', width: 300, editable: true },
+    { field: 'notes', headerName: 'Notes', width: 300, editable: true },
+    {
+      field: 'delete',
+      headerName: 'Delete',
+      width: 100,
+      renderCell: (params) => (
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      ),
+    },
+  ];
+
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        padding: 2,
+        paddingBottom: 8,
+        borderRadius: 5,
+        height: 300,
+        width: '100%',
+        marginBottom: '20px',
+        marginTop: '20px',
+        bgcolor: 'rgba(91, 130, 130, 0.4)',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+        }}
+      >
+        <IconButton color="inherit">
+          <AddIcon sx={{ color: 'white' }} />
+        </IconButton>
+      </Box>
+      <h1 style={{ color: 'white' }}>Careers</h1>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        disableRowSelectionOnClick
+        disableColumnMenu
+        hideFooterPagination
+        hideFooterSelectedRowCount
+        hideFooter
+        sx={{ borderColor: 'transparent' }}
+      />
+    </Box>
+  );
+};
+
+export default Careers;
--- client/src/components/CoverLetter.jsx
+++ client/src/components/CoverLetter.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Box, TextField } from '@mui/material';
 
 const CoverLetter = () => {
   return (
-    <div>CoverLetter</div>
-  )
-}
+    <Box
+      sx={{
+        padding: 1,
+        borderRadius: 5,
+        height: '62%',
+        marginLeft: '50px',
+        bgcolor: 'rgba(91, 130, 130, 0.4)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+        fontSize: '18px',
+      }}
+    >
+      <h3>Cover Letter Generator</h3>
+      <Box mt={4} mb={2}>
+        <p> paragraph.</p>
+      </Box>
+      <Box mt="auto" width="90%">
+        <TextField label="Enter Text" variant="outlined" fullWidth />
+      </Box>
+    </Box>
+  );
+};
 
-export default CoverLetter
+export default CoverLetter;
