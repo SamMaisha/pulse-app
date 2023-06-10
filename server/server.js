04ce74e1e9ec0de5af1d6e9839549b6545887128
Sam Maisha <sam.maisha99@gmail.com>
2023-06-10 11:19:58 -0400

    add router handler to file for quicklinks

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
@@ -5,6 +6,12 @@ const port = 8001;
 
 // Middleware
 app.use(morgan("dev"));
+
+// require routes
+const quickLinksRouter = require("./routers/quicklinks-router");
+
+// use routes
+app.use("/api/quicklinks", quickLinksRouter);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
