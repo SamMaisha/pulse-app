e6378826250906d88ef2468d237f916efc2c6f7a
Sam Maisha <sam.maisha99@gmail.com>
2023-06-10 11:18:53 -0400

    add test route and route for user to see their quicklinks when they log in

--- server/routers/quicklinks-router.js
+++ server/routers/quicklinks-router.js
@@ -2,17 +2,16 @@ const express = require("express");
 const router = express.Router();
 const quickLinksQueries = require("../db/queries/quickLinks");
 
-// GET /api/quicklinks => return raw data quicklinks
-// router.get("/", (req, res) => {
-//   quickLinksQueries.getQuickLinks().then((result) => {
-//     res.json(result);
-//   });
-// });
-
 //TEST
-
 router.get("/", (req, res) => {
   quickLinksQueries.testLink().then((result) => {
+    res.json(result);
+  });
+});
+
+//GET /api/quicklinks/:userid => return raw data quicklinks for user
+router.get("/:userid", (req, res) => {
+  quickLinksQueries.getQuickLinks().then((result) => {
     res.json(result);
   });
 });
