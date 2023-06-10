037342731a5b1acc7bd06befc19065984e45530f
Cloris Li <cloris.lzy@gmail.com>
2023-06-10 04:28:08 -0700

    update careers component, add content, add popup window for add/edit

--- client/src/components/Careers.jsx
+++ client/src/components/Careers.jsx
@@ -1,25 +1,120 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import { IconButton } from '@mui/material';
+import { IconButton, Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Checkbox } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { DataGrid } from '@mui/x-data-grid';
 import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
 
 const Careers = () => {
-  const [rows, setRows] = useState([
-    { id: 1, date: null, opportunity: '', notes: '' },
-  ]);
+  const [careers, setCareers] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedCareer, setSelectedCareer] = useState(null);
+  const [newCareer, setNewCareer] = useState({
+    company: '',
+    position: '',
+    website: '',
+    coverLetter: false,
+    applied: false,
+    interviewed: false,
+    notes: '',
+  });
+
+  const handleAddCareer = () => {
+    setSelectedCareer(null);
+    setNewCareer({
+      company: '',
+      position: '',
+      website: '',
+      coverLetter: false,
+      applied: false,
+      interviewed: false,
+      notes: '',
+    });
+    setOpen(true);
+  };
+
+
+  const handleDeleteCareer = (id) => {
+    const updatedCareers = careers.filter((career) => career.id !== id);
+    setCareers(updatedCareers);
+  };
+
+  const handleEditCareer = (career) => {
+    setSelectedCareer(career);
+    setNewCareer(career);
+    setOpen(true);
+  };
+
+  const handleSaveCareer = () => {
+    if (selectedCareer) {
+      setCareers((prevCareers) =>
+        prevCareers.map((career) =>
+          career.id === selectedCareer.id ? { ...newCareer, id: selectedCareer.id } : career
+        )
+      );
+    } else {
+      const newId = careers.length > 0 ? careers[careers.length - 1].id + 1 : 1;
+      setCareers((prevCareers) => [...prevCareers, { ...newCareer, id: newId }]);
+    }
+
+    setOpen(false);
+  };
+
+  const handleInputChange = (event, field) => {
+    const value = field === 'coverLetter' || field === 'applied' || field === 'interviewed' ? event.target.checked : event.target.value;
+
+    setNewCareer((prevCareer) => ({
+      ...prevCareer,
+      [field]: value,
+    }));
+  };
 
   const columns = [
-    { field: 'date', headerName: 'Date', width: 200, editable: true },
-    { field: 'opportunity', headerName: 'Opportunity', width: 300, editable: true },
-    { field: 'notes', headerName: 'Notes', width: 300, editable: true },
+    { field: 'company', headerName: 'Company', width: 150 },
+    { field: 'position', headerName: 'Position', width: 150 },
+    { field: 'website', headerName: 'Website', width: 200 },
+    {
+      field: 'coverLetter',
+      headerName: 'Cover Letter',
+      width: 100,
+      renderCell: (params) => (
+        <Checkbox checked={params.value} disabled />
+      ),
+    },
+    {
+      field: 'applied',
+      headerName: 'Applied',
+      width: 80,
+      renderCell: (params) => (
+        <Checkbox checked={params.value} disabled />
+      ),
+    },
+    {
+      field: 'interviewed',
+      headerName: 'Interviewed',
+      width: 100,
+      renderCell: (params) => (
+        <Checkbox checked={params.value} disabled />
+      ),
+    },
+    { field: 'notes', headerName: 'Notes', width: 200 },
+    {
+      field: 'edit',
+      headerName: 'Edit',
+      width: 70,
+      renderCell: (params) => (
+        <IconButton onClick={() => handleEditCareer(params.row)}>
+          <EditIcon />
+        </IconButton>
+      ),
+    },
     {
       field: 'delete',
       headerName: 'Delete',
-      width: 100,
+      width: 70,
       renderCell: (params) => (
-        <IconButton>
+        <IconButton onClick={() => handleDeleteCareer(params.row.id)}>
           <DeleteIcon />
         </IconButton>
       ),
@@ -35,6 +130,7 @@ const Careers = () => {
         borderRadius: 5,
         height: 300,
         width: '100%',
+        maxWidth: 750,
         marginBottom: '20px',
         marginTop: '20px',
         bgcolor: 'rgba(91, 130, 130, 0.4)',
@@ -47,13 +143,15 @@ const Careers = () => {
           right: '10px',
         }}
       >
-        <IconButton color="inherit">
+        <IconButton onClick={handleAddCareer}>
           <AddIcon sx={{ color: 'white' }} />
         </IconButton>
       </Box>
+
       <div className="title">Careers</div>
+
       <DataGrid
-        rows={rows}
+        rows={careers}
         columns={columns}
         disableRowSelectionOnClick
         disableColumnMenu
@@ -62,6 +160,65 @@ const Careers = () => {
         hideFooter
         sx={{ borderColor: 'transparent' }}
       />
+
+      {/* Popup window */}
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>{selectedCareer ? 'Edit Career' : 'Add Career'}</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Company"
+            value={newCareer.company}
+            onChange={(e) => handleInputChange(e, 'company')}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Position"
+            value={newCareer.position}
+            onChange={(e) => handleInputChange(e, 'position')}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Website"
+            value={newCareer.website}
+            onChange={(e) => handleInputChange(e, 'website')}
+            fullWidth
+            margin="normal"
+          />
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Checkbox
+              checked={newCareer.coverLetter}
+              onChange={(e) => handleInputChange(e, 'coverLetter')}
+            />
+            <span>Cover Letter</span>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Checkbox checked={newCareer.applied} onChange={(e) => handleInputChange(e, 'applied')} />
+            <span>Applied</span>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Checkbox
+              checked={newCareer.interviewed}
+              onChange={(e) => handleInputChange(e, 'interviewed')}
+            />
+            <span>Interviewed</span>
+          </Box>
+          <TextField
+            label="Notes"
+            value={newCareer.notes}
+            onChange={(e) => handleInputChange(e, 'notes')}
+            fullWidth
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={handleSaveCareer}>
+            {selectedCareer ? 'Save' : 'Add'}
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
