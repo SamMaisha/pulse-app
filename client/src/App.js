2ecefbf1dbabe14cd3e01622d620f88c427bc70c
Sam Maisha <sam.maisha99@gmail.com>
2023-06-11 11:03:16 -0400

    clean up app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Grid from '@mui/material/Grid';
-import Home from './Pages/Home';
-import Dashboard from './Pages/Dashboard';
-import Login from './Pages/Login';
-import About from './Pages/About';
-import Register from './Pages/Register';
-import Footer from './components/Footer';
-import LoginButton from './Pages/LoginButton';
-import LogoutButton from './Pages/LogoutButton';
-import Profile from './Pages/Profile';
-import './imgs/pulse.png';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Grid from "@mui/material/Grid";
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Pages/Login";
+import About from "./Pages/About";
+import Register from "./Pages/Register";
+import Footer from "./components/Footer";
+import LoginButton from "./Pages/LoginButton";
+import LogoutButton from "./Pages/LogoutButton";
+import Profile from "./Pages/Profile";
+import "./imgs/pulse.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <Grid container
-        marginTop="150px"
-        >
+    <div className="App">
+      <Grid container marginTop="150px">
         <Navbar />
         <Grid
           container
@@ -30,8 +28,16 @@ function App() {
           sx={{ padding: "50px", overflow: "hidden" }}
         >
           <Routes>
-            <Route path='/login' element={<LoginButton />} />
-            <Route path='/profile' element={<div className="profile"> <Profile /> <LogoutButton /> </div>} />
+            <Route path="/login" element={<LoginButton />} />
+            <Route
+              path="/profile"
+              element={
+                <div className="profile">
+                  {" "}
+                  <Profile /> <LogoutButton />{" "}
+                </div>
+              }
+            />
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
