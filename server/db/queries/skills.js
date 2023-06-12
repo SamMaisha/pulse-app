5567b177723f14ed17162aa5ce042d2fa8a7ad30
Sam Maisha <sam.maisha99@gmail.com>
2023-06-12 17:26:43 -0400

    create queries for adding new skill, updating skill and deleting skill

--- server/db/queries/skills.js
+++ server/db/queries/skills.js
@@ -12,10 +12,57 @@ const getSkills = function (userId) {
   });
 };
 
+// add new skill to db
+const addSkill = function (userId, name, status) {
+  const queryParams = [userId, name, status];
+  const parameterizedQuery = `
+  INSERT INTO skills (user_id, name, status)
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `;
+  return db.query(parameterizedQuery, queryParams).then((data) => {
+    console.log(data.rows);
+    return data.rows[0];
+  });
+};
+
+// update skill in db
+const updateSkill = function (userId, skillId, newName, newStatus) {
+  const queryParams = [userId, skillId, newName, newStatus];
+  const parameterizedQuery = `
+  UPDATE skills
+  SET name = $3, status = $4
+  WHERE id = $2 AND user_id = $1
+  RETURNING *
+  `;
+  return db.query(parameterizedQuery, queryParams).then((data) => {
+    console.log(data.rows[0]);
+    return data.rows[0];
+  });
+};
+
+// delete skill from db
+const deleteSkill = function (userId, skillId) {
+  const queryParams = [userId, skillId];
+  const parameterizedQuery = `
+  DELETE FROM skills
+  WHERE id = $2 AND user_id = $1
+  `;
+  return db.query(parameterizedQuery, queryParams);
+};
+
 // test get skills
 const testLinkSkills = function () {
   return db.query("SELECT * FROM skills;").then((data) => {
     console.log(data.rows);
     return data.rows;
   });
+};
+
+module.exports = {
+  testLinkSkills,
+  getSkills,
+  addSkill,
+  updateSkill,
+  deleteSkill,
 };
