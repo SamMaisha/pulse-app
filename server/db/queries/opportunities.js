6829ef6eab1c87a2efdb4f7686a3cc48e1b137a0
Sam Maisha <sam.maisha99@gmail.com>
2023-06-12 18:26:41 -0400

    update code to reference opportunities table and data instead of skills

--- server/db/queries/opportunities.js
+++ server/db/queries/opportunities.js
@@ -14,50 +14,65 @@ const getOpportunities = function (userId) {
 };
 
 // add new opportunity to db
-const addSkill = function (userId, name, status) {
-  const queryParams = [userId, name, status];
+const addOpportunity = function (userId, name, date, notes) {
+  const queryParams = [userId, name, date, notes];
   const parameterizedQuery = `
-  INSERT INTO skills (user_id, name, status)
-  VALUES ($1, $2, $3)
+  INSERT INTO opportunities (user_id, name, date, notes)
+  VALUES ($1, $2, $3, $4)
   RETURNING *
   `;
   return db.query(parameterizedQuery, queryParams).then((data) => {
-    console.log(data.rows);
+    console.log("RESULT", data.rows);
+    console.log("RESULT1", data.rows[0]);
     return data.rows[0];
   });
 };
 
-// update skill in db
-const updateSkill = function (userId, skillId, newName, newStatus) {
-  const queryParams = [userId, skillId, newName, newStatus];
+// update opportunity in db
+const updateOpportunity = function (
+  userId,
+  opportunityId,
+  newName,
+  newDate,
+  newNote
+) {
+  const queryParams = [userId, opportunityId, newName, newDate, newNote];
   const parameterizedQuery = `
-  UPDATE skills
-  SET name = $3, status = $4
+  UPDATE opportunities
+  SET name = $3, date = $4, notes = $5
   WHERE id = $2 AND user_id = $1
   RETURNING *
   `;
   return db.query(parameterizedQuery, queryParams).then((data) => {
-    console.log(data.rows);
+    console.log("1", data.rows);
+    console.log("2", data.rows[0]);
     return data.rows[0];
   });
 };
 
-// delete skill from db
-const deleteSkill = function (userId, skillId) {
-  const queryParams = [userId, skillId];
+// delete opportunity from db
+const deleteOpportunity = function (userId, opportunityId) {
+  const queryParams = [userId, opportunityId];
   const parameterizedQuery = `
-  DELETE FROM skills
+  DELETE FROM opportunities
   WHERE id = $2 AND user_id = $1
   `;
   return db.query(parameterizedQuery, queryParams);
 };
 
-// test get skills
-const testLinkSkills = function () {
-  return db.query("SELECT * FROM skills;").then((data) => {
-    console.log(data.rows);
+// test get opportunities
+const testOpportunities = function () {
+  return db.query("SELECT * FROM opportunities;").then((data) => {
+    console.log("TEST1", data.rows);
+    console.log("TEST2", data.rows[0]);
     return data.rows;
   });
 };
 
-module.exports = {};
+module.exports = {
+  testOpportunities,
+  getOpportunities,
+  addOpportunity,
+  updateOpportunity,
+  deleteOpportunity,
+};
