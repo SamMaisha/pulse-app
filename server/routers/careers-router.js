a08a801c6670989b0b51e8130fcc748b909be0c6
Sam Maisha <sam.maisha99@gmail.com>
2023-06-14 14:17:36 -0400

    copy router code from skills

--- server/routers/careers-router.js
+++ server/routers/careers-router.js
@@ -1,7 +1,57 @@
 const express = require("express");
 const router = express.Router();
+const skillsQueries = require("../db/queries/skills");
 
-// GET /api/careers => return raw data careers
-router.get("", (req, res) => {});
+//////////////////////////// TEST WITHOUT USER ID ////////////////////////////////
+
+//TEST GET
+router.get("/", (req, res) => {
+  skillsQueries.testLinkSkills().then((result) => {
+    res.json(result);
+  });
+});
+
+/////////////////////////////////////// WITH USER ID //////////////////////////////////
+
+//GET /api/skills/:userid => return raw data skills for user
+router.get("/:userid", (req, res) => {
+  const userId = req.params.userid;
+  skillsQueries.getSkills(userId).then((result) => {
+    res.json(result);
+  });
+});
+
+// PUT /api/skills/:userid/:skillid
+router.put("/:userid/:skillid", (req, res) => {
+  const userId = req.params.userid;
+  const skillId = req.params.skillid;
+  const newName = req.body.name;
+  const newStatus = req.body.status;
+  // update skill in database
+  skillsQueries
+    .updateSkill(userId, skillId, newName, newStatus)
+    .then((result) => res.json(result));
+});
+
+// POST /api/skills/:userid
+router.post("/:userid", (req, res) => {
+  const userId = req.params.userid;
+  const skillName = req.body.name;
+  const skillStatus = req.body.status;
+  // add skill to database
+  skillsQueries
+    .addSkill(userId, skillName, skillStatus)
+    .then((result) => res.status(201).json(result));
+});
+
+// DELETE /api/skills/:userid/:skillid
+router.delete("/:userid/:skillid", (req, res) => {
+  const userId = req.params.userid;
+  const skillId = req.params.skillid;
+  // delete skill from database
+  skillsQueries
+    .deleteSkill(userId, skillId)
+    .then(() => res.status(200).send("skill deleted successfully"));
+});
 
 module.exports = router;
