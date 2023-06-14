eed4db7a09ed20cffde6cc6e21c7acbe483ab256
Sam Maisha <sam.maisha99@gmail.com>
2023-06-14 14:28:55 -0400

    update routes for GET and PUT endpointe for careers

--- server/routers/careers-router.js
+++ server/routers/careers-router.js
@@ -1,55 +1,73 @@
 const express = require("express");
 const router = express.Router();
-const skillsQueries = require("../db/queries/skills");
+const careersQueries = require("../db/queries/careers");
 
 //////////////////////////// TEST WITHOUT USER ID ////////////////////////////////
 
 //TEST GET
 router.get("/", (req, res) => {
-  skillsQueries.testLinkSkills().then((result) => {
+  careersQueries.testGetCareer().then((result) => {
     res.json(result);
   });
 });
 
 /////////////////////////////////////// WITH USER ID //////////////////////////////////
 
-//GET /api/skills/:userid => return raw data skills for user
+//GET /api/careers/:userid
 router.get("/:userid", (req, res) => {
   const userId = req.params.userid;
-  skillsQueries.getSkills(userId).then((result) => {
+  careersQueries.getCareers(userId).then((result) => {
     res.json(result);
   });
 });
 
-// PUT /api/skills/:userid/:skillid
-router.put("/:userid/:skillid", (req, res) => {
+// PUT /api/careers/:userid/:careerid
+router.put("/:userid/:careerid", (req, res) => {
   const userId = req.params.userid;
-  const skillId = req.params.skillid;
-  const newName = req.body.name;
-  const newStatus = req.body.status;
-  // update skill in database
-  skillsQueries
-    .updateSkill(userId, skillId, newName, newStatus)
+  const careerId = req.params.careerid;
+  const {
+    jobTitle,
+    companyName,
+    jobLink,
+    isCoverLetterGenerated,
+    isApplied,
+    isInterviewed,
+    notes,
+  } = req.body;
+
+  // update career in database
+  careersQueries
+    .updateSkill(
+      userId,
+      careerId,
+      jobTitle,
+      companyName,
+      jobLink,
+      isCoverLetterGenerated,
+      isApplied,
+      isInterviewed,
+      notes
+    )
     .then((result) => res.json(result));
 });
 
-// POST /api/skills/:userid
+// POST /api/careers/:userid
 router.post("/:userid", (req, res) => {
   const userId = req.params.userid;
   const skillName = req.body.name;
   const skillStatus = req.body.status;
-  // add skill to database
-  skillsQueries
+  // add career to database
+  careersQueries
     .addSkill(userId, skillName, skillStatus)
     .then((result) => res.status(201).json(result));
 });
 
-// DELETE /api/skills/:userid/:skillid
+// DELETE /api/careers/:userid/:careerid
 router.delete("/:userid/:skillid", (req, res) => {
   const userId = req.params.userid;
   const skillId = req.params.skillid;
-  // delete skill from database
-  skillsQueries
+  // delete career from database
+  careersQueries
     .deleteSkill(userId, skillId)
     .then(() => res.status(200).send("skill deleted successfully"));
 });
