89a122688f1ebbfac52f6479583da047c55dc141
Daniel <danielseniv@outlook.com>
2023-06-16 19:54:22 -0400

    Page to render inbetween user being authenticated and DOM rendering

---
+++ client/src/Pages/callback-pages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Grid } from "@mui/material";
+
+export const CallbackPage = () => {
+    return (
+        <Grid container>
+            <Navbar />
+        </Grid>
+    )
+}
