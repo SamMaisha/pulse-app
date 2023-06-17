06b11f3af2c3b390a09c7dc163fdeff8af5198ad
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-06-17 10:43:39 -0600

    update navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import {
   ListItem,
   ListItemText,
   Slide,
+  Box,
 } from "@mui/material";
 import { Menu as MenuIcon } from "@mui/icons-material";
 import { Link } from "react-router-dom";
@@ -16,7 +17,6 @@ import logo from "../imgs/pulse.png";
 
 const Navbar = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
   const { logout, isAuthenticated, loginWithRedirect } = useAuth0();
 
   const toggleDrawer = () => {
@@ -40,16 +40,30 @@ const Navbar = () => {
             style={{ width: "50px", height: "auto" }}
           />
 
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer}
-          >
-            <MenuIcon />
-          </IconButton>
+          {!isAuthenticated && (
+            <Box display="flex">
+              <ListItem component={Link} to="/About">
+                <ListItemText className="title" primary="ABOUT" />
+              </ListItem>
+              <ListItem component={Link} onClick={() => loginWithRedirect()}>
+                <ListItemText className="title" primary="LOGIN" />
+              </ListItem>
+            </Box>
+          )}
+
+          {isAuthenticated && (
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
+
       {isAuthenticated && (
         <Drawer
           anchor="right"
@@ -75,32 +89,6 @@ const Navbar = () => {
             </ListItem>
             <ListItem component={Link} onClick={() => logout()}>
               <ListItemText className="title" primary="LOGOUT" />
-            </ListItem>
-          </List>
-        </Drawer>
-      )}
-      {!isAuthenticated && (
-        <Drawer
-          anchor="right"
-          open={isDrawerOpen}
-          onClose={toggleDrawer}
-          TransitionComponent={Slide}
-          TransitionProps={{
-            direction: "left",
-            timeout: 10000,
-          }}
-          sx={{
-            width: 300,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": { width: 400, backgroundColor: "#487a6c" },
-          }}
-        >
-          <List>
-            <ListItem component={Link} to="/About">
-              <ListItemText className="title" primary="ABOUT PULSE" />
-            </ListItem>
-            <ListItem component={Link} onClick={() => loginWithRedirect()}>
-              <ListItemText className="title" primary="LOGIN" />
             </ListItem>
           </List>
         </Drawer>
