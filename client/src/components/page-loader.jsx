4a13590c66102095dc8eccb1f1962dc479024b8e
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-06-17 18:15:32 -0600

    restyle page loader

--- client/src/components/page-loader.jsx
+++ client/src/components/page-loader.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
 export const PageLoader = () => {
-    const loadingImg = "https://cdn.auth0.com/blog/hello-auth0/loader.svg";
+  const loadingImg = "https://cdn.auth0.com/blog/hello-auth0/loader.svg";
 
-    return (
-        <div className="loader">
-            <img src={loadingImg} alt="Loading..." />
-        </div>
-    );
+  return (
+    <div
+      className="loader"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <img
+        src={loadingImg}
+        alt="Loading..."
+        style={{
+          width: "20%",
+          opacity: 0.5,
+          animation: "spin 5s linear infinite",
+        }}
+      />
+      <style>
+        {`
+          @keyframes spin {
+            from { transform: rotate(0deg); }
+            to { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </div>
+  );
 };
