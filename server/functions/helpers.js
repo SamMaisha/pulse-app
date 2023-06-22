f0377d397471e7105ac12c627ce31a970f43dfdb
Daniel <danielseniv@outlook.com>
2023-06-21 22:20:11 -0400

    Add comments for logic

--- server/functions/helpers.js
+++ server/functions/helpers.js
@@ -1,16 +1,15 @@
 const { getAllUserIds } = require('../db/queries/users')
 
-const userValidator = function(userId) {
+const userValidator = function(userId) { //takes in userId, checks if the id is unique or not in database
     return getAllUserIds()
     .then(data => {
         for(const user of data)  {
             if (userId === user.auth0_id) {
-                const result = true;
+                const result = true; //returns true if match is found
                 return result;
             }
         }
-        const result = false;
-        return false;
+        return false; //returns false if no match found
     })
 }
 
